6fc26845ca33214c249722286667756af271f1ac
Jörg Prante <joergprante@gmail.com>
2015-10-28 17:52:05 +0100

    beginning retention policy

---
+++ src/main/java/org/xbib/elasticsearch/action/retain/Retention.java
@@ -0,0 +1,145 @@
+package org.xbib.elasticsearch.action.retain;
+
+import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequestBuilder;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.admin.indices.get.GetIndexResponse;
+import org.elasticsearch.common.hppc.cursors.ObjectCursor;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.ESLoggerFactory;
+import org.elasticsearch.common.settings.Settings;
+import org.xbib.elasticsearch.support.client.Ingest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Retention {
+
+    private final static ESLogger logger = ESLoggerFactory.getLogger(Retention.class.getName());
+
+    private final Settings settings;
+
+    private final Ingest ingest;
+
+    public Retention(Settings settings, Ingest ingest) {
+        this.settings = settings;
+        this.ingest = ingest;
+    }
+
+    public String resolveAlias(String alias) {
+        if (ingest.client() == null) {
+            return alias;
+        }
+        GetAliasesResponse getAliasesResponse = ingest.client().admin().indices().prepareGetAliases(alias).execute().actionGet();
+        if (!getAliasesResponse.getAliases().isEmpty()) {
+            return getAliasesResponse.getAliases().keys().iterator().next().value;
+        }
+        return alias;
+    }
+
+    public void updateAliases(String index, String concreteIndex) {
+        if (ingest.client() == null) {
+            return;
+        }
+        if (!index.equals(concreteIndex)) {
+            IndicesAliasesRequestBuilder requestBuilder = ingest.client().admin().indices().prepareAliases();
+            GetAliasesResponse getAliasesResponse = ingest.client().admin().indices().prepareGetAliases(index).execute().actionGet();
+            if (getAliasesResponse.getAliases().isEmpty()) {
+                logger.info("adding alias {} to index {}", index, concreteIndex);
+                requestBuilder.addAlias(concreteIndex, index);
+                // identifier is alias
+                if (settings.get("identifier") != null) {
+                    requestBuilder.addAlias(concreteIndex, settings.get("identifier"));
+                }
+            } else {
+                for (ObjectCursor<String> indexName : getAliasesResponse.getAliases().keys()) {
+                    if (indexName.value.startsWith(index)) {
+                        logger.info("switching alias {} from index {} to index {}", index, indexName.value, concreteIndex);
+                        requestBuilder.removeAlias(indexName.value, index)
+                                .addAlias(concreteIndex, index);
+                        if (settings.get("identifier") != null) {
+                            requestBuilder.removeAlias(indexName.value, settings.get("identifier"))
+                                    .addAlias(concreteIndex, settings.get("identifier"));
+                        }
+                    }
+                }
+            }
+            requestBuilder.execute().actionGet();
+            if (settings.getAsBoolean("retention.enabled", false)) {
+                performRetentionPolicy(
+                        index,
+                        concreteIndex,
+                        settings.getAsInt("retention.diff", 48),
+                        settings.getAsInt("retention.mintokeep", 2));
+            }
+        }
+    }
+
+    public void performRetentionPolicy(String index, String concreteIndex, int timestampdiff, int mintokeep) {
+        if (ingest.client() == null) {
+            return;
+        }
+        if (index.equals(concreteIndex)) {
+            return;
+        }
+        GetIndexResponse getIndexResponse = ingest.client().admin().indices()
+                .prepareGetIndex()
+                .execute().actionGet();
+        Pattern pattern = Pattern.compile("^(.*?)(\\d+)$");
+        List<String> indices = new ArrayList<>();
+        logger.info("{} indices", getIndexResponse.getIndices().length);
+        for (String s : getIndexResponse.getIndices()) {
+            Matcher m = pattern.matcher(s);
+            if (m.matches()) {
+                if (index.equals(m.group(1)) && !s.equals(concreteIndex)) {
+                    indices.add(s);
+                }
+            }
+        }
+        if (indices.isEmpty()) {
+            logger.info("no indices found, retention policy skipped");
+            return;
+        }
+        if (mintokeep > 0 && indices.size() < mintokeep) {
+            logger.info("{} indices found, not enough for retention policy ({}),  skipped",
+                    indices.size(), mintokeep);
+            return;
+        } else {
+            logger.info("candidates for deletion = {}", indices);
+        }
+        List<String> indicesToDelete = new ArrayList<String>();
+        // our index
+        Matcher m1 = pattern.matcher(concreteIndex);
+        if (m1.matches()) {
+            Integer i1 = Integer.parseInt(m1.group(2));
+            for (String s : indices) {
+                Matcher m2 = pattern.matcher(s);
+                if (m2.matches()) {
+                    Integer i2 = Integer.parseInt(m2.group(2));
+                    int kept = 1 + indices.size() - indicesToDelete.size();
+                    if (timestampdiff > 0 && i1 - i2 > timestampdiff && mintokeep <= kept) {
+                        indicesToDelete.add(s);
+                    }
+                }
+            }
+        }
+        logger.info("indices to delete = {}", indicesToDelete);
+        if (indicesToDelete.isEmpty()) {
+            logger.info("not enough indices found to delete, retention policy complete");
+            return;
+        }
+        String[] s = indicesToDelete.toArray(new String[indicesToDelete.size()]);
+        DeleteIndexRequestBuilder requestBuilder = ingest.client().admin().indices().prepareDelete(s);
+        DeleteIndexResponse response = requestBuilder.execute().actionGet();
+        if (!response.isAcknowledged()) {
+            logger.warn("retention delete index operation was not acknowledged");
+        }
+    }
+
+}
+
+
+
